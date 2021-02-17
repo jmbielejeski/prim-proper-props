d6cfdbe1653ca40fc531c0351658d048887dd0f6
jmbielejeski <jmbielejeski@gmail.com>
2021-02-17 15:56:04 -0700

    finished silverware stretch goal

--- src/components/DinnerSupplies/DinnerSupplies.js
+++ src/components/DinnerSupplies/DinnerSupplies.js
@@ -1,16 +1,11 @@
+import SilverWare from '../SilverWare/SilverWare'
+
 function DinnerSupplies({guestList}) {
   return (
   <>  
     <h2>Dinner Supplies</h2>
-      <div>
-        Spoons: {guestList.length * 2}
-      </div>
-      <div>
-        Forks: {guestList.length * 2}
-      </div>
-      <div>
-        Knives: {guestList.length * 2}
-      </div>
+    <SilverWare count={guestList} />
+
   </>    
   );
 }
---
+++ src/components/SilverWare/SilverWare.js
@@ -0,0 +1,17 @@
+function SilverWare ({count}) {
+  return (
+    <>
+      <div>
+        Spoons: {count.length * 2}
+      </div>
+      <div>
+        Forks: {count.length * 2}
+      </div>
+      <div>
+        Knives: {count.length * 2}
+      </div>
+    </>
+  )
+}
+
+export default SilverWare;
